49d0775cc308602c6632c624ddc0ca9f2b460924
Peejay Garcia <peejgarcia@gmail.com>
2019-02-11 17:28:38 -0600

    Added title and body columns to database

--- main.py
+++ main.py
@@ -1,6 +1,50 @@
 from flask import Flask, request, redirect, render_template
-from flask_squlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config('DEBUG') = True
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
 
+class Blog(db.Model): 
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(1000))
+    completed = db.Column(db.Boolean)
+
+    def __init__(self, name):
+        self.name = name
+        self.completed = False
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    if request.method == 'POST':
+        blog_name = request.form['blog']
+        new_blog = Blog(blog_name)
+        db.session.add(new_blog)
+        db.session.commit()
+
+    blogs = Blog.query.filter_by(completed=False).all()
+    completed_blogs = Blog.query.filter_by(completed=True).all()
+    return render_template('todos.html', title="Build-A-Blog", 
+        blogs=blogs, completed_blogs=completed_blogs)
+
+
+@app.route('/delete-blog', methods=['POST'])
+def delete_blog():
+
+    blog_id = int(request.form['blog-id'])
+    blog = Blog.query.get(blog_id)
+    blog.completed = True
+    db.session.add(blog)
+    db.session.commit()
+
+    return redirect('/')    
+
+
+if __name__ == '__main__':
+    app.run()
