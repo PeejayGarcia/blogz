0774f943d82a445e576521b314849251eeb3cb2c
Peejay Garcia <peejgarcia@gmail.com>
2019-02-26 20:30:37 -0600

    test

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-app.secret_key = 'Ampongan'
 
 class Blog(db.Model): 
 
